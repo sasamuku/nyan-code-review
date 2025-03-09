31d0ebc5212c993188d36bf27f97d12dfdaa6573
sasamuku <sasazawa3330@gmail.com>
2025-03-09 15:29:43 +0900

    Replace GitHub logo with personal avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,17 +14,18 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com"
+            href="https://github.com/sasamuku"
             target="_blank"
             rel="noopener noreferrer"
           >
             By{' '}
             <Image
-              src="/github-mark.svg"
-              alt="GitHub Logo"
+              src="/sasamuku-avatar.png"
+              alt="sasamuku Avatar"
               width={24}
               height={24}
               priority
+              className="rounded-full"
             />
           </a>
         </div>
